5ed8fbe26d07a7eebb9fbeed3e63db4ea5a8a6e3
Matthew Campbell <ematthewcampbell@gmail.com>
2016-07-17 19:53:09 -0600

    can sign in with spotify

--- Gemfile
+++ Gemfile
@@ -28,7 +28,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'unicorn'
 
 gem 'rails_12factor', group: :production
-
+gem 'omniauth-oauth2'
+gem "figaro"
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   resources :songs
   resources :playlists
+  get '/auth/spotify/callback', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
